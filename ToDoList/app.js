7e3368602ca44dc70374d5d0139d4f32eff55a13
aadi-Code-7 <shr.aadi000@gmail.com>
2026-03-07 10:02:43 +0530

    Local Storage added

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,6 +1,12 @@
 let addBtn = document.querySelector("#addBtn");
 let taskText = document.querySelector('input');
-let list = document.querySelector(".taskList")
+let list = document.querySelector(".taskList");
+
+list.innerHTML = localStorage.getItem("tasks" || "");
+
+function saveTasks(){
+    localStorage.setItem("tasks", list.innerHTML);
+}
 
 addBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -30,13 +36,34 @@ addBtn.addEventListener("click", (e) => {
     span.append(taskDiv,btnDiv);
     list.append(span);
 
+    saveTasks();
+
     taskText.value = "";
 
-    deleteButton.addEventListener("click", () => {
-        span.remove();
-    });
-    doneButton.addEventListener("click", () => {
+    // deleteButton.addEventListener("click", () => {
+    //     span.remove();
+    //     saveTasks();
+    // });
+    // doneButton.addEventListener("click", () => {
+    //     taskDiv.style.textDecoration = "line-through";
+    //     taskDiv.style.opacity = "0.3";
+    //     saveTasks();
+    // });
+
+});
+
+list.addEventListener("click", (e) => {
+
+    if(e.target.classList.contains("deleteBtn")){
+        e.target.parentElement.parentElement.remove();
+        saveTasks();
+    }
+
+    if(e.target.classList.contains("doneBtn")){
+        let taskDiv = e.target.parentElement.previousElementSibling;
         taskDiv.style.textDecoration = "line-through";
         taskDiv.style.opacity = "0.3";
-    });
+        saveTasks();
+    }
+
 });
